c7896437dad874e9c69f9e22fc7bd0b98e08fc7c
palkovics adam <adam@debian-BULLSEYE-live-builder-AMD64>
2022-11-28 00:18:00 +0100

    beugrok 20210105 - read file

--- beugrok/20210105/src/Main.java
+++ beugrok/20210105/src/Main.java
@@ -1,10 +1,20 @@
 import patogenetika.Baktérium;
 
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        File input = new File(args[1]);
-        System.out.println(input);
+        try {
+            File file = new File(args[0]);
+            System.out.println(file.getAbsolutePath());
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String data = sc.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.printf("Error during opening: %s\n",e.getMessage());
+        }
     }
 }
